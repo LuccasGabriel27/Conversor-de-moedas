8a7ee2dccaf855d1651d14a8d4847a3362d127fe
LuccasGabriel27 <luccasgabriel862@gmail.com>
2024-05-22 09:30:49 -0300

    Fazendo o backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,16 @@
 const express = require('express');
-const app = express()
 
-app
+const aplicacao = express();
+
+aplicacao.get('/', (req, res) => {
+    res.send("Chamei o backend com sucesso")
+});
+
+aplicacao.post('/', (req, res) => {
+    res.send("Chamei o backend com sucesso usando post")
+});
+
+aplicacao.listen(4000, () => {
+    console.log("Escutando na porta 4000")
+});
+    
