2acd7e945d853150ff16abb8395199b71d5de85d
LuccasGabriel27 <luccasgabriel862@gmail.com>
2024-05-22 10:26:55 -0300

    Fazendo att  do backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const aplicacao = express();
+const port = 4000;
 
 aplicacao.get('/', (req, res) => {
     res.send("Chamei o backend com sucesso")
@@ -10,7 +10,28 @@ aplicacao.post('/', (req, res) => {
     res.send("Chamei o backend com sucesso usando post")
 });
 
-aplicacao.listen(4000, () => {
+aplicacao.get('/moedas', (req,res) => {
+
+    const moedas = {
+        BRL: "real",
+        DOLAR: "dolar",
+        EURO: "euro"
+    }
+
+    res.status(200).json(moedas);
+
+});
+
+aplicacao.get('/info', (req,res) => {
+    res.send("Informação sobre o sistema");
+});
+
+
+
+
+
+
+// Aplicação ouvindo a porta 4000
+aplicacao.listen(port, () => {
     console.log("Escutando na porta 4000")
 });
-    
