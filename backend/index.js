d4c55d882d9f9cc77290f8e2c6519f8da466571d
LuccasGabriel27 <luccasgabriel862@gmail.com>
2024-05-22 10:50:14 -0300

    Fazendo atts do backend

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,46 @@ aplicacao.get('/info', (req,res) => {
     res.send("Informação sobre o sistema");
 });
 
+aplicacao.post('/moedas', (req,res) => {
 
+    const moedas = {
+        BRL: "real",
+        DOLAR: "dolar",
+        EURO: "euro"
+    }
+
+    res.status(200).json(moedas);
+
+});
+
+aplicacao.get('/info', (req,res) => {
+
+    const informacoes = {
+        version: "1.0",
+        author: "Luccas",
+        update: "Maio de 2024",
+        price: "free",
+        license: "ABC"
+    }
+    
+res.status(200).json(moedas);
+
+});
+
+aplicacao.get('/conversao/ :moedas', (req, res) => {
+
+    //  processo de conversao
+    let moedas = req.params.moedas.split("-");
+    let moeda1 = moeda[0];
+    let moeda2 = moeda[1];
+
+    console.log(moeda1);
+    console.log(moeda2);
+
+    conversao = {};
+    res.status(200).json(conversao);
+
+})
 
 
 // Aplicação ouvindo a porta 4000
